4ecef2ca83c0be345a09aa91ffe1a2da285d538c
manqing <chenmanqing@baidu.com>
2017-08-18 17:55:49 +0800

    update file's path

--- tools/createQRcode/createQRcode.js
+++ tools/createQRcode/createQRcode.js
@@ -12,7 +12,7 @@ module.exports = function (content, dir) {
         const ip = "http://" + getIP + ":" + dir;
         QRCode.toDataURL(ip, {scale: 8}, function (err, url) {
             const qrImg = `<img src="${url}">`;            
-            const $ = cheerio.load(fs.readFileSync('./tools/createQRcode/createQRcode.html', "utf-8"));
+            const $ = cheerio.load(fs.readFileSync(path.join(__dirname ,'/createQRcode.html')), "utf-8");
             $('.bird-tools_qrcode-content').append(qrImg);
             content += $.html();
             resolve(content);
